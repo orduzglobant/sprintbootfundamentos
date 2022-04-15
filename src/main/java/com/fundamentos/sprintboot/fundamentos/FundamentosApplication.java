6c110c79d4032d83406b8221037286be79250c78
admin <ti@ospedale.com.co>
2022-04-15 10:29:02 -0500

    (11) Inyección de dependencia "Component"
    Creamos nuestra propia dependencia, bajo el estereotipo de component y segregar la implementacion en dos clases, la implementacion de una interfaz, la anotacion @Qualifier("componentTwoImplement") me permite seleccionar que clase quiero implementar.

--- src/main/java/com/fundamentos/sprintboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentos/sprintboot/fundamentos/FundamentosApplication.java
@@ -1,13 +1,31 @@
 package com.fundamentos.sprintboot.fundamentos;
 
+import com.fundamentos.sprintboot.fundamentos.component.ComponentDependency;
+import com.fundamentos.sprintboot.fundamentos.component.ComponentImplement;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FundamentosApplication {
+public class FundamentosApplication implements CommandLineRunner {
+
+	private ComponentDependency componentDependency;
+
+	@Autowired
+	public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency) {
+		this.componentDependency = componentDependency;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FundamentosApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		componentDependency.saludar();
+		ComponentImplement componentImplement = new ComponentImplement();
+		componentImplement.saludar();
+	}
 }
